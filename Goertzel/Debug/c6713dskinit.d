5fb62fc15f89dbb708b2759470337795d8752be1
Amina ait lafkih <114859285+aminal22@users.noreply.github.com>
2024-04-17 00:12:29 +0100

    code

---
+++ Goertzel/Debug/c6713dskinit.d
@@ -0,0 +1,39 @@
+# FIXED
+
+c6713dskinit.obj: ../c6713dskinit.c
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/Support/C6713dskinit.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/dsk6713/include/dsk6713.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_chip.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_stdinc.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_stdinchal.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/tistdtypes.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_chiphal.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_emifhal.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_chip.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_irq.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_irqhal.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_timer.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_timerhal.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/dsk6713/include/dsk6713_aic23.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_mcbsp.h
+c6713dskinit.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_mcbsphal.h
+
+../c6713dskinit.c: 
+D:/ti_v6_2/CCCStudio_v3.1/Support/C6713dskinit.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/dsk6713/include/dsk6713.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_chip.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_stdinc.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_stdinchal.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/tistdtypes.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_chiphal.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_emifhal.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_chip.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_irq.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_irqhal.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_timer.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_timerhal.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/dsk6713/include/dsk6713_aic23.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_mcbsp.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_mcbsphal.h: 
---
+++ Goertzel/Debug/main.d
@@ -0,0 +1,35 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/dsk6713/include/dsk6713_aic23.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_chip.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_stdinc.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_stdinchal.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/tistdtypes.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_chiphal.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_emifhal.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_chip.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_irq.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_irqhal.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_timer.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_timerhal.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_mcbsp.h
+main.obj: D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_mcbsphal.h
+
+../main.c: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/dsk6713/include/dsk6713_aic23.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_chip.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_stdinc.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_stdinchal.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/tistdtypes.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_chiphal.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_emifhal.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_chip.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_irq.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_irqhal.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_timer.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_timerhal.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_mcbsp.h: 
+D:/ti_v6_2/CCCStudio_v3.1/C6000/csl/include/csl_mcbsphal.h: 
